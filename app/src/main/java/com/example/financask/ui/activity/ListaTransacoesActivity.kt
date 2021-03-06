263bb5773f06feeceff12c40ff060b1371ec8466
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-06 14:29:50 -0300

    add class ListTransacoesActivity and folder res

---
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.financask.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
