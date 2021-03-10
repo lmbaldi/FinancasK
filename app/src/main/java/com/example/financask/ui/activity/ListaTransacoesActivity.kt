0d463390d45842d6ba8a6d249e698ae3e588249f
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-09 21:07:57 -0300

    layout files are changed to work with Android X

--- app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,10 +2,12 @@ package com.example.financask.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.financask.R
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
     }
 }
