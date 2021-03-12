12d2c7e6c82ccf3eddbfe02ed577d11673c12969
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-03-12 15:39:56 -0300

    add class ListaTransacoesAdapter

--- app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,13 +1,23 @@
 package com.example.financask.ui.activity
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.financask.R
+import com.example.financask.ui.adapter.ListaTransacoesAdapter
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 20,50", "Economia - R$ 100,00" )
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes, this))
     }
 }
